9e3266195e88c25fc6a8db79ea064b5a5aaaab96
Anton <a.ptashynskyi@gmail.com>
2021-11-05 15:49:36 +0100

    add hw-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
-function getRandomHexColor() {
+
+const colorBtn = document.querySelector('.change-color');
+const nameOfColor = document.querySelector('.color');
+const colorOfBody = document.querySelector('body')
+
+colorBtn.addEventListener('click', handleNameOfColor);
+colorBtn.addEventListener('click', handleBGColor);
+
+
+function handleNameOfColor(event) { 
+  nameOfColor.textContent = changeColorBtn();
+}
+
+function handleBGColor(params) {
+  colorOfBody.style.backgroundColor = changeColorBtn();
+}
+
+function changeColorBtn() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
