236b660672affaae116cab27b5ec138512c68c81
Anton <a.ptashynskyi@gmail.com>
2021-11-05 15:10:44 +0100

    add hw-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', handelFormSubmit);
 
+function handelFormSubmit(event) {
+  event.preventDefault();
+  const mail = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+  const formData = { mail, password };
+
+  if ((mail || password) === '') {
+    alert('All fields should be filled!');
+  } else {
+    loginForm.reset();
+    return console.log(formData);
+  }
+};
+
+
+// We can use also FormaData method
+
+// const formData = new FormData(event.currentTarget);
+//   console.log(formData);
+//   formData.forEach((value, name) => {
+//     console.log(name);
+//     console.log(value);
+//   })
