7e745320eaf5e9c8d24fd0173a666855429a3660
Anton <a.ptashynskyi@gmail.com>
2021-11-07 14:54:31 +0100

    update hw-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,34 +3,28 @@ const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const box = document.querySelector('#boxes');
 
-
 createBtn.addEventListener('click', createBoxes);
 destroyBtn.addEventListener('click', destroyBoxes);
 
-
 function createBoxes() {
-  let inputV = input.value
+  destroyBoxes();
 
-  let size = 30;
-
-  for (let i = 0; i < inputV; i += 1) {
+  for (let i = 0; i < input.value; i += 1) {
     const container = document.createElement('div');
-    
+    const size = 30 + i * 10;
+
     container.style.width = `${size}px`;
     container.style.height = `${size}px`;
     container.style.backgroundColor = getRandomHexColor();
-    size += 10;
-    
-    box.insertAdjacentElement("beforeend", container);
-  }
 
-};
+    box.insertAdjacentElement('beforeend', container);
+  }
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+}
 
 function destroyBoxes(event) {
   box.innerHTML = '';
 }
-
